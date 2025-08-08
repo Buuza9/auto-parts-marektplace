01428169611871bdfa4c3dfba44d718f11c589fd
Mohammed Salah <buuza.salah99@gmail.com>
2025-08-08 15:05:39 +0200

    some modification has been made to the product model.

---
+++ models/product.py
@@ -0,0 +1,14 @@
+from app import db
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    store_id = db.Column(db.Integer, db.ForeignKey("store.id"), nullable=False)
+    name = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, default=0)
+    barcode = db.Column(
+        db.String(50), unique=True, nullable=False
+    )  # barcode for each product
+    image_url = db.Column(db.String(255))  # optional product image URL
